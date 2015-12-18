699e0cc56f486f85f17228b46bc8b4d73445942a
MattMcMurray <mmcmurray@visualspection.com>
2015-12-18 13:22:24 -0600

    Created utility to parse config.json files

---
+++ config.py
@@ -0,0 +1,21 @@
+import json
+from pprint import pprint
+
+filename='config.json'
+
+config = None
+
+try:
+	with open(filename, 'r') as f:
+		config = json.load(f)
+except:
+	print '[ERROR] Couldn\'t load config file: {0}'.format(filename)
+
+def getSSH():
+	return config['ssh']
+	if config is not None and 'ssh' in config:
+		return config['ssh']
+
+if __name__ == '__main__':
+	pprint(getSSH())
+
