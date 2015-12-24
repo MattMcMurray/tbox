68d1dce0d2c78f37e6d8b5fa9498a1a00228e2b5
MattMcMurray <mmcmurray@visualspection.com>
2015-12-24 16:13:51 -0600

    Added click listeners to all buttons

---
+++ web/static/js/index.js
@@ -0,0 +1,31 @@
+$('#startDeluge').click(function () {
+	console.log("Start deluge");
+}); 
+
+$('#stopDeluge').click(function () {
+	console.log("Stop deluge");
+});
+
+$('#restartDeluge').click(function () {
+	console.log("Restart deluge");
+});
+
+$('#startSamba').click(function () {
+	console.log("Start samba");
+});
+
+$('#stopSamba').click(function () {
+	console.log("Stop samba");
+});
+
+$('#restartSamba').click(function () {
+	console.log("Restart samba");
+});
+
+$('#shutdown').click(function () {
+	console.log("Shutdown the pi");
+});
+
+$('#reboot').click(function () {
+	console.log("Reboot the pi");
+});
