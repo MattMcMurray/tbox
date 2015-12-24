998411d8abaa7cf3a57687e7456cc91dc5aa1431
MattMcMurray <mmcmurray@visualspection.com>
2015-12-24 15:47:55 -0600

    More API methods

--- web/run.py
+++ web/run.py
@@ -14,25 +14,63 @@ def run_command(cmd):
 def index():
 	return render_template('index.html')
 
+
+####################################################
+####################   API   #######################
+####################################################
 @app.route('/api/start/<service>', methods=['POST'])
 def api_start(service):
+	status = 500
 	data = {
-		'cmd': '',
-		'stdout': ''
 	}
 
 	print service
 	if service == 'samba':
-		data['cmd'] = 'ls'
-		data['stdout'] = run_command('ls')
+		cmd = 'sudo service samba start'
+		data['cmd'] = cmd
+		data['stdout'] = run_command(cmd)
+		status = 200
 	elif service == 'deluge':
-		data['stdout'] = data['cmd'] = 'ls'
-		run_command('ls')
+		cmd = 'deluged'
+		data['cmd'] = cmd
+		data['stdout'] = run_command(cmd)
+		status = 200
+	else:
+		data['error'] = "No service {0}".format(service)
 
 	js = json.dumps(data)
-	resp = Response(js, status=200, mimetype='application/json')
+	resp = Response(js, status=status, mimetype='application/json')
 
 	return resp
+
+@app.route('/api/stop/<service>', methods=['POST'])
+def api_stop(service):
+	status = 500
+	data = {
+	}
+
+	print service
+	if service == 'samba':
+		cmd = 'sudo service samba stop'
+		data['cmd'] = cmd
+		data['stdout'] = run_command(cmd)
+		status = 200
+	elif service == 'deluge':
+		cmd = 'pkill deluged'
+		data['cmd'] = cmd
+		data['stdout'] = run_command(cmd)
+		status = 200
+	else:
+		data['error'] = "No service {0}".format(service)
+
+	js = json.dumps(data)
+	resp = Response(js, status=status, mimetype='application/json')
+
+	return resp
+
+@app.route('/api/restart/<service>', methods=['POST'])
+def api_restart(service):
+	print ''
 
 if __name__ == '__main__':
 	app.debug = True
